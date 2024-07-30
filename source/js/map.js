d190b891cb5d87e422e0d6bd7f65ff58b18409ef
aleksandr_gvozdev <wayk5i@yandex.ru>
2024-07-30 17:13:34 +0300

    Вносит правки.

--- source/js/map.js
+++ source/js/map.js
@@ -1,13 +1,17 @@
+let map = document.querySelector('.contacts__map-wrapper--active');
+
+map.classList.remove('contacts__map-wrapper--nojs');
+
 ymaps.ready(init);
             function init() {
               var myMap = new ymaps.Map("map", {
                 center: [59.938631, 30.323037],
                 zoom: 14
               }, {
-                searchControlProvider: 'yandex#search'
+                searchControlProvider: "yandex#search"
               });
               var myPlacemark = new ymaps.Placemark([59.938631, 30.323037], null, {
-                iconLayout: 'default#image',
+                iconLayout: "default#image",
                 iconImageHref: "img/map/map-pin.svg",
                 iconImageSize: [57, 53],
                 iconImageOffset: [-29, -53]
